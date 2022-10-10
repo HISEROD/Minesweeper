5ceac79fdd8a91a46501f24ebc387917b47d765a
Immanuel Hise <hisebeast@gmail.com>
2022-10-10 16:36:51 -0500

    move grid methods from main file to grid.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,35 +1,8 @@
 ﻿import time
-import random
 from string import ascii_lowercase
 
 from config import *
 from grid import *
-
-# set_nums will count and assign the no. of neighboring mines for each tile
-def set_nums():
-    for y in range(size_y):
-        for x in range(size_x):
-            if get_tile(x, y).mine:
-                get_tile(x, y).num = 9
-            else:
-                for y2 in range(3):
-                    for x2 in range(3):
-                        if (0 <= y+y2-1 < size_y) and (0 <= x+x2-1 < size_x) and not (y2 == 1 and x2 == 1):
-                            if get_tile(x+x2-1, y+y2-1).mine:
-                                get_tile(x, y).num += 1
-
-
-# assign mines' locations
-def lay():
-    m = mines
-    tmpx = 0
-    tmpy = 0
-    while m > 0:
-        tmpx = random.randint(0, size_x - 1)
-        tmpy = random.randint(0, size_y - 1)
-        if not get_tile(tmpx, tmpy).mine and (not tmpy in range(y-1,y+2) or not tmpx in range(x-1,x+2)):
-            get_tile(tmpx, tmpy).mine = True
-            m -= 1
 
 def you_win():
     print('''
@@ -47,25 +20,6 @@ def you_win():
  ▓▓▓  ▓▓▓   ▓▓  ▓▓  ▓▓▓
  ▓▓    ▓▓   ▓▓  ▓▓   ▓▓ ▓▓
 ''')
-
-def domino(y,x):
-    '''this function implements the domino effect which is
-seen when a tile which doesn't border any mines is picked'''
-    global mines
-    list = []
-    y -= 1
-    x -= 1
-    for i in range(3):
-        for j in range(3):
-            if (0 <= y+i < size_y) and (0 <= x+j < size_x) and not get_tile(x+j, y+i).vis:
-                get_tile(x+j, y+i).vis = True
-                if get_tile(x+j, y+i).flag:
-                    get_tile(x+j, y+i).flag = False
-                    mines += 1
-                if get_tile(x+j, y+i).num == 0:
-                    list.append([y+i,x+j])
-    for i in range(len(list)):
-        domino(list[i][0],list[i][1])
 
 letters = ascii_lowercase
 top_scale = ' ' * len(str(size_y))
