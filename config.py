cb0f00245ea81699d5f880ad8f752830130022e6
Immanuel Hise <hisebeast@gmail.com>
2022-10-22 13:14:42 -0500

    simplify comments

--- config.py
+++ config.py
@@ -5,11 +5,5 @@ size_y = 8
 # 20% chance of mine
 _density = 1 / 6
 
-# set the number of mines based on the # of
-# available tiles times the density factor
+# set the number of flags == to the # of mines
 flags = round(size_x * size_y * _density)
-
-'''this var is called flags because after
-laying the mines (see "lay" function in grid.py),
-it is used exclusively for tracking the number of
-flags as an aid esp. at the end of the game'''
